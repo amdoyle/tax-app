49d2944ce9e6d6200c10b3507f85f2d4d77fef13
Ann Marie Doyle <am.doyle@yahoo.ca>
2020-02-06 21:12:10 -0500

    react: adding moved file and package lock

--- tax-calculator/src/index.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import AppRoutes from "./app_routes";
-
-const App = () => {
-  return <AppRoutes />;
-};
-
-ReactDOM.render(<App />, document.getElementById("app"));
